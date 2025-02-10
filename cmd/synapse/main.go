428bf8e66bfa86016fc07f79535412b714303fd1
Weerakoon Thisara Rathnayaka <thisara.weerakoon2001@gmail.com>
2025-02-10 13:57:11 +0530

    Changed main() to wait until the shutdown signal receives

--- cmd/synapse/main.go
+++ cmd/synapse/main.go
@@ -18,6 +18,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"fmt"
 
 	"github.com/apache/synapse-go/internal/app/synapse"
 )
@@ -26,4 +27,6 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
 	synapse.Run(ctx)
+	<-ctx.Done()
+	fmt.Println("\nReceived shutdown signal, cleaning up...")
 }
